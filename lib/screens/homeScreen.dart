42457527b3cee04375794659bfbd19fc2a3aa521
vagnertondelo <vagnertondello@gmail.com>
2024-10-23 19:25:33 -0300

    update

---
+++ lib/screens/homeScreen.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final _formKey = GlobalKey<FormState>();
+
+  final TextEditingController _nomeController = TextEditingController();
+  final TextEditingController _descricaoController = TextEditingController();
+  final TextEditingController _precoController = TextEditingController();
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+
+        title: Text("Formulario"),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  TextFormField(
+                    controller: _nomeController,
+                    decoration: InputDecoration(
+                        labelText: 'Produto',
+                        border: OutlineInputBorder()
+                    ),
+                    validator: (value) {
+                      if(value == null || value.isEmpty){
+                        return 'Por favor, insira umm Produto';
+                      }
+                      return null;
+                    },
+                  ),
+                  SizedBox(height:10),
+                  TextFormField(
+                    controller: _descricaoController,
+                    decoration: InputDecoration(
+                        labelText: 'Descrição',
+                        border: OutlineInputBorder()
+                    ),
+                    keyboardType: TextInputType.text,
+                    validator: (value) {
+                      if(value == null || value.isEmpty){
+                        return 'Por favor, insira descrição produto';
+                      }
+
+                      return null;
+                    },
+                  ),
+                  SizedBox(height:10),
+                  TextFormField(
+                    controller: _precoController,
+                    decoration: InputDecoration(
+                        labelText: 'Preço',
+                        border: OutlineInputBorder()
+                    ),
+                    keyboardType: TextInputType.number,
+                    validator: (value) {
+                      if(value == null || value.isEmpty){
+                        return 'Por favor, insira um valor';
+                      }
+                      return null;
+                    },
+                  ),
+                  SizedBox(height: 20,),
+                  ElevatedButton(
+                    onPressed: ()  {
+
+                    },
+                    child: const Text('Enviar'),
+                  ),
+
+                ],
+              )
+          ),
+
+        ],
+      )
+    );
+  }
+}
